de87bc77828f909c455375f6adf0401f3a784728
Daniel Rojas <danielr95@hotmail.com>
2026-05-18 09:12:01 -0600

    actualiza a better sql

--- servidor.js
+++ servidor.js
@@ -12,18 +12,19 @@ app.get("/", (req, res) => {
 
 // 🔥 POST: guardar marcador
 app.post("/marcadores", (req, res) => {
+try {
     const m = req.body;
 
-    const query = `
+    const stmt = db.prepare(`
         INSERT INTO marcadores (nombre, idPartido, casa, visita, golesCasa, golesVisita, uid)
         VALUES (?, ?, ?, ?, ?, ?, ?)
         ON CONFLICT(uid, idPartido)
         DO UPDATE SET
             golesCasa=excluded.golesCasa,
             golesVisita=excluded.golesVisita
-    `;
+    `);
 
-    db.run(query, [
+    stmt.run(
         m.nombre,
         m.idPartido,
         m.casa,
@@ -31,31 +32,32 @@ app.post("/marcadores", (req, res) => {
         m.golesCasa,
         m.golesVisita,
 	m.uid
-    ], function (err) {
-        if (err) {
-            console.log("❌ Error:", err);
-            return res.status(500).json({ error: "Error guardando" });
-        }
+    );
 
         console.log("✅ Guardado en DB:", m);
         res.json({ ok: true });
-    });
+} catch (err) {
+        console.log("❌ Error:", err);
+        res.status(500).json({ error: "Error guardando" });
+    }
 });
 
 
 // 📥 GET: obtener todos los marcadores
 app.get("/marcadores", (req, res) => {
-    db.all("SELECT * FROM marcadores", [], (err, rows) => {
-        if (err) {
-            console.log("❌ Error:", err);
-            return res.status(500).json({ error: "Error leyendo" });
-        }
-
+    try {
+        const rows = db.prepare("SELECT * FROM marcadores").all();
         res.json(rows);
-    });
+
+    } catch (err) {
+        console.log("❌ Error:", err);
+        res.status(500).json({ error: "Error leyendo" });
+    }
 });
 
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
